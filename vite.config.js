c6951cb01851e0c97ee255f64f6cfe1500f56802
Ivan Miloslavov <ivan@cyberiam.ca>
2025-03-01 11:34:01 -0500

    Add a bunch of config files from React Starter Kit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
         }),
+        react(),
     ],
+    esbuild: {
+        jsx: 'automatic',
+    },
 });
